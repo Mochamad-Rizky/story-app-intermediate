2e137032c5932ea89dbb6c017fdfa65af38e3e0b
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-04-02 14:29:04 +0700

    add form app component into add story page

--- src/scripts/pages/AddStory/AddStory.js
+++ src/scripts/pages/AddStory/AddStory.js
@@ -1,12 +1,10 @@
 import { html } from 'lit';
 import LitWithoutShadowDom from '../../components/LitWithoutShadowDom/LitWithoutShadowDom';
+import '../../components/Form/Form';
 
 class AddStory extends LitWithoutShadowDom {
   render() {
-    return html`
-      <h1>Add Story</h1>
-      <p>Add Story page</p>
-    `;
+    return html` <form-app class="bg-nikel d-block px-5 py-4 rounded"></form-app> `;
   }
 }
 
