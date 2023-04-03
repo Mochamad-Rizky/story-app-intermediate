ce877071e3ee25f867058cea8a39734e410c532d
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-04-03 12:21:57 +0700

    add not found page styles

--- src/scripts/pages/NotFound/NotFound.js
+++ src/scripts/pages/NotFound/NotFound.js
@@ -1,12 +1,25 @@
-import { html } from 'lit';
-import LitWithoutShadowDom from '../../components/LitWithoutShadowDom/LitWithoutShadowDom';
+import { css, html, LitElement } from 'lit';
+import '../../components/Error/Error';
 
-class NotFound extends LitWithoutShadowDom {
-  render() {
-    return html`
-      <h1>Not Found</h1>
-      <p>Not Found page</p>
+class NotFound extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        padding: 2rem;
+        background-color: #212529;
+        border-radius: 1rem;
+      }
+
+      h2 {
+        color: white;
+        text-align: center;
+      }
     `;
+  }
+
+  render() {
+    return html` <h2>Page Not Found</h2> `;
   }
 }
 
