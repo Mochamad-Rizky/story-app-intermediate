ae9fe5e6cfb587d68b3a5235fbc51e4a673d1568
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-04-03 12:22:11 +0700

    refactor dashboard page

--- src/scripts/pages/Dashboard/Dashboard.js
+++ src/scripts/pages/Dashboard/Dashboard.js
@@ -4,11 +4,18 @@ import LitWithoutShadowDom from '../../components/LitWithoutShadowDom/LitWithout
 import renderSkeletonUICard from '../../utils/renderSkeletonUICard';
 
 import '../../components/StoryCardItem/StoryCardItem';
+import '../../components/Error/Error';
 
 class Dashboard extends LitWithoutShadowDom {
   static get properties() {
     return {
       data: {
+        type: Object,
+      },
+      isLoading: {
+        type: Boolean,
+      },
+      isError: {
         type: Object,
       },
     };
@@ -21,38 +28,41 @@ class Dashboard extends LitWithoutShadowDom {
 
   async #fetchData() {
     try {
+      this.isLoading = true;
       const { listStory } = await api.getStories();
       this.data = listStory;
     } catch (error) {
-      console.log(error);
+      this.isError = {
+        status: true,
+        message: error.message,
+      };
+    } finally {
+      this.isLoading = null;
     }
   }
 
   render() {
-    let storiesDataElement;
-    if (this.data) {
-      storiesDataElement = html`
-        ${this.data.map(
-          ({ id, name, description, photoUrl, createdAt }) =>
-            html` <story-card-item
-              class="col-lg-4 col-md-6"
-              id=${id}
-              name=${name}
-              description=${description}
-              image=${photoUrl}
-              createdAt=${createdAt}
-            />`,
-        )}
-      `;
-    } else {
-      storiesDataElement = html` ${renderSkeletonUICard(6)} `;
-    }
-
     return html`
-      <div class="p-3">
-        <div class="p-1">
-          <h2 class="text-white">List Story</h2>
-          <div class="row position-relative">${storiesDataElement}</div>
+      <div class="p-4 h-auto">
+        <h2 class="text-white">List Story</h2>
+        <div class="row position-relative d-flex align-items-center justify-content-center">
+          ${this.data &&
+          !this.isLoading &&
+          this.data?.map(
+            ({ id, name, description, photoUrl, createdAt }) => html`
+              <story-card-item
+                class="col-lg-4 col-md-6"
+                id=${id}
+                name=${name}
+                description=${description}
+                image=${photoUrl}
+                createdAt=${createdAt}
+              />
+            `,
+          )}
+          ${this.isLoading && renderSkeletonUICard(6)}
+          ${this.isError?.status &&
+          html`<error-app class="mt-5" message=${this.isError.message}></error-app>`}
         </div>
       </div>
     `;
