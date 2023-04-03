eba30e168051232d634452930bc14a16da19b5bb
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-04-03 18:42:48 +0700

    add localization build result

---
+++ src/generated/locales/id.js
@@ -0,0 +1,30 @@
+
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
+
+    
+    
+
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's136b0e562d93a8f0': `Dikembangkan oleh`,
+'s1f30f2eaf1e38fb9': `Tambah Cerita`,
+'s2a309fabbfbf6e26': `Mohon isi sebuah deskripsi.`,
+'s2b46a91307f0ed8f': `Mohon pilih sebuah file`,
+'s38f2e04876af7406': `Deskripsi cerita*`,
+'s3c30b8ed468a13b3': `Terlihat Bagus`,
+'s3f1046395b601bb3': `Kontak saya:`,
+'s5e8250fb85d64c23': `Tutup`,
+'s7fee95b66225e1fb': `Dasbor`,
+'s8b81550ee72bf3e8': `Daftar Cerita`,
+'s8daa1db5187d4ad4': `Selengkapnya`,
+'s985bc233a2ae259f': `Pilih file:`,
+'sa6873df757e9f393': `Cerita berhasil di tambahkan`,
+'sb3d4f79d9d8b71e5': `Kirim`,
+'sb72c988ee15f790b': `Gagal menambahkan cerita`,
+'sc1663ad8bc983cc8': `Deskripsi cerita`,
+'sec77d4da6a57f2b7': `Halaman tidak ditemukan`,
+    };
+  
