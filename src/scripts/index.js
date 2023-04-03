30b74d920ca070b5ef2e7bbc6f93ba7dedb435cc
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-04-03 17:13:28 +0700

    add dom laoded set locale function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,14 @@
 import App from './app';
 import ActiveLinkInitiator from './utils/activeLinkInitiator';
-import checkLang from './utils/checkLang';
+import checkLang from './utils/lang';
 
 import '../styles/main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 // components
 import './components/Navbar/Navbar';
 import './components/Footer/Footer';
+import './components/LocalePicker/LocalePicker';
+import { setLocaleFromUrl } from './utils/localization';
 
 const app = new App();
 const activeLinkInitiator = new ActiveLinkInitiator();
@@ -18,6 +20,7 @@ window.addEventListener('DOMContentLoaded', async () => {
   links = document.querySelectorAll('app-button[renderType="link"][class="nav-link"]');
   links = Array.from(links).map((link) => link.shadowRoot.querySelector('a'));
   activeLinkInitiator.update(links);
+  setLocaleFromUrl();
 });
 
 window.addEventListener('hashchange', async () => {
