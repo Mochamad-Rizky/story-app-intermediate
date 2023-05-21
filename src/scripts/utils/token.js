8fc980650dd02790dc5a830586358ada338a7993
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-05-21 15:15:14 +0700

    refactor token utils

--- src/scripts/utils/token.js
+++ src/scripts/utils/token.js
@@ -1,3 +1,7 @@
-const getToken = () => localStorage.getItem('token');
+const Token = {
+  get: () => localStorage.getItem('token'),
+  delete: () => localStorage.removeItem('token'),
+  set: (token) => localStorage.setItem('token', token),
+};
 
-export default getToken;
+export default Token;
