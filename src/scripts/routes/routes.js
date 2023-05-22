99f041fb79ef7b614e73dc8272bb9634d8243f04
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-05-22 15:54:59 +0700

    add login, story guest, and register route

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -11,9 +11,21 @@ const pages = {
     is: 'add-story-page',
     component: async () => (await import('../pages/AddStory/AddStory')).default,
   },
+  '/add-story/guest': {
+    is: 'add-story-page-guest',
+    component: async () => (await import('../pages/GuestUserMode/GuestUserMode')).default,
+  },
   '/not-found': {
     is: 'not-found-page',
     component: async () => (await import('../pages/NotFound/NotFound')).default,
+  },
+  '/login': {
+    is: 'login-page',
+    component: async () => (await import('../pages/Login/Login')).default,
+  },
+  '/register': {
+    is: 'register-page',
+    component: async () => (await import('../pages/Register/Register')).default,
   },
 };
 
