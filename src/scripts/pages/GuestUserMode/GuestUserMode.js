98182e8187b29de23289856bc0657bae42228cd6
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-05-22 15:59:50 +0700

    add guest user mode page

---
+++ src/scripts/pages/GuestUserMode/GuestUserMode.js
@@ -0,0 +1,20 @@
+import { html } from 'lit';
+import LitWithoutShadowDom from '../../components/LitWithoutShadowDom/LitWithoutShadowDom';
+import '../../components/Form/Form';
+import { msg, updateWhenLocaleChanges } from '@lit/localize';
+
+class AddStoryAsGuest extends LitWithoutShadowDom {
+  constructor() {
+    super();
+    updateWhenLocaleChanges(this);
+  }
+
+  render() {
+    return html`
+      <h1 class="text-white text-center">${msg('Guest Mode')}</h1>
+      <form-app guestMode class="bg-nikel d-block px-5 py-4 rounded"></form-app>
+    `;
+  }
+}
+
+export default AddStoryAsGuest;
