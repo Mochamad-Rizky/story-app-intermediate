5b13a131fa92ea4723639ed7da8c70c5b19b70a9
Mochamad Rizky <moch.rizky.cahya@gmail.com>
2023-05-22 15:56:37 +0700

    add getstories, detail story,post, login, and register api

--- src/scripts/utils/api.js
+++ src/scripts/utils/api.js
@@ -1,32 +1,113 @@
+import instance from '../lib/axios';
+import Token from './token';
+
 const api = (() => {
-  const getStories = () =>
-    new Promise((resolve, reject) => {
-      const fetchTimeoutInSecond = 3000; // for demo purpose only
+  const getStories = async () => {
+    try {
+      const response = await instance.get('/stories', {
+        headers: {
+          Authorization: `Bearer ${Token.get()}`,
+        },
+      });
 
-      setTimeout(async () => {
-        try {
-          const stories = await import('../data/DATA.json');
-          const storiesJson = await stories.default;
-          resolve(storiesJson);
-        } catch (error) {
-          reject(error);
-        }
-      }, fetchTimeoutInSecond);
-    });
+      return response.data;
+    } catch (error) {
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
 
-  const postStory = () =>
-    new Promise((resolve, reject) => {
-      const postTimeoutInSecond = 2000; // for demo purpose only
+  const getDetailStory = async (id) => {
+    try {
+      const response = await instance.get(`/stories/${id}`, {
+        headers: {
+          Authorization: `Bearer ${Token.get()}`,
+        },
+      });
 
-      setTimeout(() => {
-        resolve('success');
-        // reject('something went wrong');
-      }, postTimeoutInSecond);
-    });
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
+
+  const postStory = async ({ description, photo }) => {
+    try {
+      const response = await instance.post(
+        '/stories',
+        {
+          description,
+          photo,
+        },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${Token.get()}`,
+          },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
+
+  const postStoryAsGuest = async ({ description, photo }) => {
+    try {
+      const response = await instance.post(
+        '/stories/guest',
+        {
+          description,
+          photo,
+        },
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        },
+      );
+
+      return response.data;
+    } catch (error) {
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
+
+  const login = async ({ email, password }) => {
+    try {
+      const response = await instance.post('/login', {
+        email,
+        password,
+      });
+
+      return response.data;
+    } catch (error) {
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
+
+  const register = async ({ name, email, password }) => {
+    try {
+      const response = await instance.post('/register', {
+        name,
+        email,
+        password,
+      });
+
+      return response.data;
+    } catch (error) {
+      throw new Error(JSON.stringify(error.response));
+    }
+  };
 
   return {
     getStories,
     postStory,
+    login,
+    register,
+    getDetailStory,
+    postStoryAsGuest,
   };
 })();
 
